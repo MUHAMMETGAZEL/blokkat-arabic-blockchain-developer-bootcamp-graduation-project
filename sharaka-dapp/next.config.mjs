38bb64e38f9a372995eabf7b38246aad0c813f54
MUHAMMETGAZEL <112321778+MUHAMMETGAZEL@users.noreply.github.com>
2025-06-01 01:37:57 +0300

    Update next.config.mjs

--- sharaka-dapp/next.config.mjs
+++ sharaka-dapp/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+   basePath: '/blokkat-arabic-blockchain-developer-bootcamp-graduation-project'
+};
 
 export default nextConfig;
