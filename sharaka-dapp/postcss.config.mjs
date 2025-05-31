c8795ff5b4ffdb5b150ad8fe88644a2069000056
MUHAMMETGAZEL <112321778+MUHAMMETGAZEL@users.noreply.github.com>
2025-06-01 01:56:37 +0300

    Update postcss.config.mjs

--- sharaka-dapp/postcss.config.mjs
+++ sharaka-dapp/postcss.config.mjs
@@ -1,11 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  basePath: '/blokkat-arabic-blockchain-developer-bootcamp-graduation-project',
-  images: {
-    unoptimized: true
-  }
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
 
-export default nextConfig;
+export default config;
